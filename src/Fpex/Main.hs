c41e885c8b01f4bbf4556c9fe9a55f27b8b15914
Samuel Pilz <samuel.pilz@posteo.net>
2019-08-10 19:24:10 +0200

    make the grade command course-option-aware

--- src/Fpex/Main.hs
+++ src/Fpex/Main.hs
@@ -1,27 +1,37 @@
 module Fpex.Main where
 
 import           Options.Applicative
-import qualified Data.Text.Lazy                as L
+import qualified Data.Text.Lazy                as LT
 import qualified Data.Text.IO                  as T
 import           Data.Aeson.Text                ( encodeToLazyText )
 import           Data.Aeson                     ( decodeFileStrict' )
+import           Control.Monad                  ( forM_ )
+
 import           Fpex.Options
 import           Fpex.Course.Types
 import qualified Fpex.User.Simple              as User
 import qualified Fpex.User.Types               as User
-import           Fpex.Eval.Main
+import           Fpex.Eval.Main                as Eval
+import           Fpex.Eval.Pretty              as Eval
 
 defaultMain :: IO ()
 defaultMain = do
     Options { .. } <- execParser options
     -- TODO: error effect
-    Just course <- decodeFileStrict' optionCourseFile :: IO (Maybe Course)
+    Just course@Course{..} <- decodeFileStrict' optionCourseFile :: IO (Maybe Course)
+    print course
 
     case optionCommand of
-        Eval CommandGrade {..} -> do
+        Grade CommandGrade {..} -> do
             Just testSuite <- decodeFileStrict' testSuiteFile
-            testReport     <- evalStudent testSuite student
-            putStrLn . L.unpack . encodeToLazyText $ testReport
+            let students = case optionStudent of
+                   Just s -> [s]
+                   Nothing -> courseStudents
+            forM_ students $ \student@Student{..} -> do
+                T.putStrLn $ "eval student " <> matrNr
+                testReport <- evalStudent testSuite student
+                T.putStrLn $ prettyTestReport testReport
+                putStrLn ""
 
         User UserManagementCommand {..} ->
 
