77ef5a7a7622eb53fac156fe8483e3fb5edca42b
fendor <power.walross@gmail.com>
2019-11-11 15:47:45 +0100

    Add test for testsuite.json

--- tests/Course.hs
+++ tests/Course.hs
@@ -2,6 +2,7 @@ module Course where
 
 import           Test.Tasty.Hspec
 import           Fpex.Course.Types
+import           Fpex.Eval.Types
 import           Data.Aeson                     ( decodeFileStrict' )
 
 courseEmpty :: Course
@@ -41,17 +42,54 @@ course3 = Course { courseName       = "course3"
                  , courseUserPrefix = "c3-"
                  }
 
+fibTestSuite :: TestSuite
+fibTestSuite = TestSuite
+    { assignmentName  = "assignment1"
+    , testSuiteGroups =
+        [ TestGroup
+            { label         = "Fibonacci tests"
+            , pointsPerTest = Points { getPoints = 5 }
+            , penalty       = Points { getPoints = 0 }
+            , maximal       = Points { getPoints = 26 }
+            , group = [ TestCase { query = "fib 0", expectedOutput = "1" }
+                      , TestCase { query = "fib 1", expectedOutput = "1" }
+                      , TestCase { query = "fib 2", expectedOutput = "2" }
+                      , TestCase { query = "fib 3", expectedOutput = "3" }
+                      , TestCase { query = "fib 4", expectedOutput = "5" }
+                      , TestCase { query = "fib 5", expectedOutput = "8" }
+                      ]
+            }
+        , TestGroup
+            { label         = "Factorial tests"
+            , pointsPerTest = Points { getPoints = 10 }
+            , penalty       = Points { getPoints = 0 }
+            , maximal       = Points { getPoints = 30 }
+            , group         =
+                [ TestCase { query = "factorial 3", expectedOutput = "6" }
+                , TestCase { query = "factorial 0", expectedOutput = "1" }
+                , TestCase { query          = "factorial 10"
+                           , expectedOutput = "3628800"
+                           }
+                ]
+            }
+        ]
+    }
+
+
 spec :: Spec
 spec = describe "validate testdata course" $ do
     it "courseEmpty.json valid" $ do
         json <- decodeFileStrict' "testdata/courseEmpty.json"
-        json `shouldBe` (Just courseEmpty)
+        json `shouldBe` Just courseEmpty
     it "course1.json valid" $ do
         json <- decodeFileStrict' "testdata/course1.json"
-        json `shouldBe` (Just course1)
+        json `shouldBe` Just course1
     it "course2.json valid" $ do
         json <- decodeFileStrict' "testdata/course2.json"
-        json `shouldBe` (Just course2)
+        json `shouldBe` Just course2
     it "course3.json valid" $ do
         json <- decodeFileStrict' "testdata/course3.json"
-        json `shouldBe` (Just course3)
+        json `shouldBe` Just course3
+    it "testsuite-fib valid" $ do
+        json <- decodeFileStrict' "testdata/testsuite-fib.json"
+        json `shouldBe` Just fibTestSuite
