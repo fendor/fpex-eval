c5d9d0048473ec3838b72a700c1d45f7cea16bde
Samuel Pilz <samuel.pilz@posteo.net>
2020-02-09 12:56:59 +0100

    remove hunit and implement assertEqual

--- fpex-test-spec/TestSpec.hs
+++ fpex-test-spec/TestSpec.hs
@@ -2,23 +2,23 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module TestSpec where
 
 import           Data.Maybe                     ( fromMaybe )
 import           System.Timeout                 ( timeout )
 import           System.IO                      ( FilePath )
-import           Test.HUnit.Lang                ( performTestCase
-                                                , Assertion
-                                                )
 import           GHC.Generics                   ( Generic )
 import           Data.Aeson                     ( FromJSON
                                                 , ToJSON
                                                 )
+import           Control.Exception             as E
+import           Control.Monad                  ( unless )
+import           Data.Typeable                  ( Typeable )
 import qualified Data.Aeson.Encode.Pretty      as Aeson
 import qualified Data.ByteString.Lazy          as BL
-
-import qualified Test.HUnit.Lang               as HUnit
+import           Control.DeepSeq                ( deepseq )
 
 group :: TestGroupProps -> [TestCase] -> TestGroup
 group = TestGroup
@@ -26,10 +26,22 @@ group = TestGroup
 testSuite :: [TestGroup] -> TestSuite
 testSuite = TestSuite
 
+assertEqual :: (Show a, Eq a) => a -> a -> IO ()
+assertEqual left right =
+    unless (left == right) $ leftStr `deepseq` rightStr `deepseq` E.throw
+        (ExpectedButGot leftStr rightStr)
+  where
+    leftStr  = show left
+    rightStr = show right
+
 type Points = Int
 type TimeoutSecs = Int
 
-type TestCase = Assertion
+data ExpectedButGot = ExpectedButGot String String
+    deriving (Eq, Show, Typeable, Generic)
+    deriving anyclass (FromJSON, ToJSON, E.Exception)
+
+type TestCase = IO ()
 
 data TestGroupProps = TestGroupProps
     { label :: String
@@ -50,7 +62,7 @@ newtype TestSuite = TestSuite
 
 data TestCaseResult
     = TestCaseResultOk
-    | TestCaseResultNok String
+    | TestCaseResultExpectedButGot ExpectedButGot
     | TestCaseResultException String
     | TestCaseResultTimeout
     deriving (Eq, Show, Generic)
@@ -84,15 +96,19 @@ writeTestSuiteResults outFile = BL.writeFile outFile . Aeson.encodePretty
 
 -- runners
 
-runTestCase :: TimeoutSecs -> Assertion -> IO TestCaseResult
+runTestCase :: TimeoutSecs -> TestCase -> IO TestCaseResult
 runTestCase timeoutSecs testCase = fromMaybe TestCaseResultTimeout <$> timeout
     (timeoutSecs * 1000 * 1000)
-    (hUnitResultToTestCaseResult <$> performTestCase testCase)
-
-hUnitResultToTestCaseResult :: HUnit.Result -> TestCaseResult
-hUnitResultToTestCaseResult HUnit.Success       = TestCaseResultOk
-hUnitResultToTestCaseResult (HUnit.Failure _ s) = TestCaseResultNok s
-hUnitResultToTestCaseResult (HUnit.Error   _ s) = TestCaseResultException s
+    (           (testCase >> return TestCaseResultOk)
+    `E.catches` [ E.Handler (return . TestCaseResultExpectedButGot)
+                -- Re-throw AsyncException
+                , E.Handler (throw :: E.AsyncException -> IO a)
+                , E.Handler
+                    (\(e :: E.SomeException) ->
+                        return $ TestCaseResultException $ show e
+                    )
+                ]
+    )
 
 runTestGroup :: TimeoutSecs -> TestGroup -> IO TestGroupResults
 runTestGroup timeoutSecs TestGroup {..} = do
