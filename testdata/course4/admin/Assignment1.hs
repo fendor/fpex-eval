83fdc3a8a938473325fde6a402d3b6770f69f783
Samuel Pilz <samuel.pilz@posteo.net>
2020-02-09 12:58:55 +0100

    update test assignment file

--- testdata/course4/admin/Assignment1.hs
+++ testdata/course4/admin/Assignment1.hs
@@ -1,11 +1,15 @@
-
 import           TestSpec
-import           Test.HUnit                     ( (@?=) )
 import           Assignment1
 
 main = do
     result <- runTestSuite 5 $ testSuite
-        [ group (TestGroupProps "" 5 0 26)
-                [fib 0 @?= 1, fib 1 @?= 1, fib 2 @?= 2, fib 3 @?= 3, fib 4 @?= 5]
+        [ group
+              (TestGroupProps "" 5 0 26)
+              [ fib 0 `assertEqual` 1
+              , fib 1 `assertEqual` 1
+              , fib 2 `assertEqual` 2
+              , fib 3 `assertEqual` 3
+              , fib 4 `assertEqual` 5
+              ]
         ]
     writeTestSuiteResults "results.out" result
