557f96a6d007caaaf3fa648b910a2bce5d1f110f
Fendor <power.walross@gmail.com>
2021-03-14 14:46:54 +0100

    Add golden tests for test-report

--- tests/Main.hs
+++ tests/Main.hs
@@ -2,9 +2,10 @@ module Main where
 
 import GradeResultParser (goldenParserTests)
 import Test.Tasty
+import Report (goldenReportTests)
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Fpex Tests" [goldenParserTests]
+tests = testGroup "Fpex Tests" [goldenParserTests, goldenReportTests]
---
+++ tests/Report.hs
@@ -0,0 +1,27 @@
+module Report where
+
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Text.Encoding as Text
+import Fpex.Publish.Plain (prettyTestReport)
+import System.FilePath
+import Test.Tasty
+import Test.Tasty.Golden
+
+goldenReportTests :: TestTree
+goldenReportTests = testGroup "Generate Report" $
+  flip map reportTestNames $ \input ->
+    goldenVsString ("Parse: " ++ input) (goldenReportRoot </> input -<.> "golden") $ do
+      r <- Aeson.eitherDecodeFileStrict (goldenReportInputRoot </> input)
+      case r of
+        Left err -> pure $ LBS.pack err
+        Right res -> pure $ LBS.fromStrict $ Text.encodeUtf8 $ prettyTestReport res
+
+reportTestNames :: [FilePath]
+reportTestNames = ["basic.json", "timeout.json", "overflow.json"]
+
+goldenReportRoot :: FilePath
+goldenReportRoot = "testdata/goldenReport"
+
+goldenReportInputRoot :: FilePath
+goldenReportInputRoot = "testdata/goldenReport/input"
